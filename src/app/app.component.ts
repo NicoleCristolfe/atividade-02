982b743c0f763ffd2b727d1d3e23c35f292341b7
Nicole Cristolfe <nicole.silva@labldi.com.br>
2024-09-10 00:26:11 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgFor,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'atividade-2';
+  value = '';
+  recebe: any = [];
+  desabilitado: any = [];
+
+  
+  removeItem(item: string): void {
+    const remove = this.recebe.indexOf(item);
+    if (remove > -1) {
+      this.recebe.splice(remove, 1);
+    }
+  }
+
+  addItem(): void {
+    if (!this.value) {
+      return;
+    }
+    this.recebe.push(this.value);
+    this.value = '';
+  }
 }
